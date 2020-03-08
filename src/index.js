241b81dae95b77a5475a517dd71307a5bcc793d3
Rafael Alves <rbalves.ads@gmail.com>
2020-03-08 12:36:54 -0400

    Add apidoc in route home api

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true,}));
 app.use(express.static("public"));
-
+app.use('/', express.static("public/apidoc"));
 const db = require('./db/queries');
 
 /**
